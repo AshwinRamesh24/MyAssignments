3673ec75886e0da6eebff76438eec1099652875d
AshwinRamesh24 <96662962+AshwinRamesh24@users.noreply.github.com>
2026-02-12 01:29:28 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -8,22 +8,22 @@ public class FibonacciSeries {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the range: ");
-		int range = sc.nextInt();
-		int f = 0;
-		int s = 1;
+		int num1 = 0;
+		int num2 = 1;
 		
-		System.out.println("Fibonacci Series: ");
-		for (int i = 0; i < range; i++) {
-			System.out.println(f+ " ");
-			int next = f + s;
-			f = s;
-			s = next;
+		int numAdd ;
+		int lengthofSeries = 10;
+		for (int i = 0; i<lengthofSeries; i++) 
+		{
+			//first print num1 as per logic
+			System.out.print(num1 + " ");
+			numAdd = num1 + num2;
+			num1 = num2;
+			num2 = numAdd;
+			
 			
 		}
-		sc.close();
-			
+		
 		
 		
 
